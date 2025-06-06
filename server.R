bd2263131122aeb19224ff199ddb3face93a7da0
Ergin Çağatay ÇANKAYA <ergincagataycankaya@gmail.com>
2025-06-06 00:33:39 -0600

    ff

--- server.R
+++ server.R
@@ -20,7 +20,7 @@ server <- function(input, output, session) {
         overlayGroups = names(lulc_urls),
         options = layersControlOptions(collapsed = FALSE)
       ) %>%
-      leaflet.extras2::addSplitMap(left, right) %>%
+      # leaflet.extras2::addSplitMap(left, right) %>%
       addResetMapButton() %>%
       setView(lng = 29.0, lat = 41.1, zoom = 12)
   })
